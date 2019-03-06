d351aa0b1bd869c918e0f388be4a0f1568e243f4
Harold <haroldcj95@gmail.com>
2019-03-05 23:54:30 -0400

    refactor extract play from performances

--- statement.js
+++ statement.js
@@ -1,23 +1,28 @@
-function amountFor(play, perf) {
-  let amount=0;
+function amountFor(play, aPerformance) {
+  let result=0;
   switch (play.type) {
     case "tragedy":
-      amount = 40000;
-      if (perf.audience > 30) {
-        amount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
-      amount = 30000;
-      if (perf.audience > 20) {
-        amount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      amount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
-  return amount;
+  return result;
+}
+
+
+function playFor(plays, perf) {
+  return plays[perf.playID];
 }
 
 function statement (invoice, plays) {
@@ -28,18 +33,17 @@ function statement (invoice, plays) {
                       { style: "currency", currency: "USD",
                         minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
 
-    let thisAmount = amountFor(play, perf);
+    let thisAmount = amountFor(playFor(plays, perf), perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type)
+    if ("comedy" ===  playFor(plays, perf).type)
       volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+    result += ` ${ playFor(plays, perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
 
