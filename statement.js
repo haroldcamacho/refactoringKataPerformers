7feb84b2da0042679c3576038fdaefbdd5129ad2
Harold <haroldcj95@gmail.com>
2019-03-05 21:27:16 -0400

    refactor switch from Statement Function

--- statement.js
+++ statement.js
@@ -1,3 +1,25 @@
+function amountFor(play, perf) {
+  let amount=0;
+  switch (play.type) {
+    case "tragedy":
+      amount = 40000;
+      if (perf.audience > 30) {
+        amount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      amount = 30000;
+      if (perf.audience > 20) {
+        amount += 10000 + 500 * (perf.audience - 20);
+      }
+      amount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return amount;
+}
+
 function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -9,23 +31,7 @@ function statement (invoice, plays) {
     const play = plays[perf.playID];
     let thisAmount = 0;
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    thisAmount = amountFor(play, perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
