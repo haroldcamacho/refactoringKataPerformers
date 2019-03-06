96e86d5cba384d2d83b8999abed87c8b482ac2c8
Harold <haroldcj95@gmail.com>
2019-03-06 10:48:36 -0400

    refactor volume credits

--- statement.js
+++ statement.js
@@ -28,9 +28,9 @@ function playFor(perf) {
 
 
 function statement (invoice, plays) {
-  let totalAmount = 0;
   playsGlobal = plays;
-  let volumeCredits = 0;
+
+  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -38,6 +38,8 @@ function statement (invoice, plays) {
     result += ` ${ playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
+
+  let volumeCredits = 0;
   for (let perf of invoice.performances){
     volumeCredits+=volumeCreditsFor(perf);
   }
