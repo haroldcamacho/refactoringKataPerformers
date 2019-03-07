0a49a57609207a8630ce28503f21e62bb6c5bacd
Harold <haroldcj95@gmail.com>
2019-03-07 09:47:49 -0400

    refactor volumeCredits into class

--- createStatementData.js
+++ createStatementData.js
@@ -27,6 +27,14 @@ class PerformanceCalculator {
         }
         return result;
     }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type) result +=
+            Math.floor(this.performance.audience / 5);
+        return result;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -43,8 +51,8 @@ function enrichPerformance(aPerformance){
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result=Object.assign({},aPerformance);
     result.play = calculator.play;
-    result.amount=amountFor(result);
-    result.volumeCredits=volumeCreditsFor(result);
+    result.amount=calculator.amount;
+    result.volumeCredits= calculator.volumeCredits;
     return result;
 }
 function playFor(aPerformance) {
