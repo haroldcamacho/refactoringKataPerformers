1b65f6c05807ac0b110afab9ff591c4df2568cd7
Harold <haroldcj95@gmail.com>
2019-03-07 09:29:05 -0400

    reorder statement.js

--- statement.js
+++ statement.js
@@ -1,8 +1,10 @@
 var invoiceGlobal;
 import createStatementData from './createStatementData.js';
 
-
-
+function statement (invoice, plays) {
+  invoiceGlobal=invoice;
+  return renderPlainText(createStatementData(invoice, plays));
+}
 
 function renderPlainText(data , plays){
   let result = `Statement for ${data.customer}\n`;
@@ -15,18 +17,31 @@ function renderPlainText(data , plays){
   return result;
 }
 
-
-function statement (invoice, plays) {
-  invoiceGlobal=invoice;
-  return renderPlainText(createStatementData(invoice, plays));
+function htmlStatement (invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
 }
 
-
+function renderHtml (data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `
+<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is
+<em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em>
+credits</p>\n`;
+  return result;
+}
 
 function  usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-          minimumFractionDigits: 2 }).format(aNumber/100);
+  return new Intl.NumberFormat("en-US",
+      { style: "currency", currency: "USD",
+        minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
 export default statement;
